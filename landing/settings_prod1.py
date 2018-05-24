e0ed2afcfe42d6a86586430148af0966c4ba8a33
Golota <golotamm@gmail.com>
2018-05-24 14:40:05 +0300

    Settings_prod1

---
+++ landing/settings_prod1.py
@@ -0,0 +1,14 @@
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbapp',
+        'USER': 'english',
+        'PASSWORD': 'misterbean',
+        'HOST': 'localhost',
+        'PORT':'',
+    }
+}
