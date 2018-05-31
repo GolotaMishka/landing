bc236a7492340d5e123759ebe954b41b48331590
Golota <golotamm@gmail.com>
2018-05-31 05:51:16 +0300

    Settings

--- landing/settings_prod1.py
+++ landing/settings_prod1.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db1',
-        'USER': 'mysite',
+        'USER': 'landing',
         'PASSWORD': 'misterbean101',
         'HOST': 'localhost',
         'PORT': '',                      # Set to empty string for default.
