90b8f1e2d4060dde6547e68f1bc63753a5a43a34
Golota <golotamm@gmail.com>
2018-06-04 20:43:02 +0300

    collectstatic2

---
+++ static/static_prod/js/valideForm.js
@@ -0,0 +1,24 @@
+$(".formValidation").on("submit", function(e){
+  
+  
+  var hasError = false;
+  
+  $(".inputValidation").each(function(){
+    var $this = $(this);
+    
+    if($this.val() == "" || $this.val() == null  ){
+      hasError = true;
+      $this.addClass("inputError");
+      
+      e.preventDefault();
+    }if($this.val() != "" && $this.val() != null){
+      $this.removeClass("inputError"); 
+      console.log($this.val())
+    }else{
+      console.log($this.val())
+      return true; 
+    }
+  }); //Input
+  
+  
+}); //Form .submit
