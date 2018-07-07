514008e5592ee630b1ceaacbd20f7b94e8213dcd
Golota <golotamm@gmail.com>
2018-07-07 05:20:23 +0300

    change pages

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ def landing(request):
 
     form = SubscriberForm(request.POST or None)
 
+
     if request.method == "POST" and form.is_valid():
         print (request.POST)
         print (form.cleaned_data)
