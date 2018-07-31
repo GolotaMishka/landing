6c0f590a78a1be32c41c87348f3a3e59b310595d
Golota <golotamm@gmail.com>
2018-07-31 22:37:26 +0300

    change Views

--- home/urls.py
+++ home/urls.py
@@ -3,9 +3,6 @@ from django.contrib import admin
 from home import views
 
 urlpatterns = [
-    url(r'^$', views.landing, name='landing'),
-    url(r'^individual/$', views.curse1, name='curse1'),
-    url(r'^group/$', views.curse2, name='curse2'),
-    url(r'^individual/#form_section$', views.curse1, name='curse1'),
-    url(r'^group/#form_section$', views.curse2, name='curse2'),
+    url(r'^$', views.curse1, name='curse1'),
+    url(r'^#form_section$', views.curse1, name='curse1'),
 ]
